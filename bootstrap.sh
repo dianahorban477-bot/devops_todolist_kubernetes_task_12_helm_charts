abcecab6c01e57fe556b3b30d41be7ecb9ebcbbc
dianahorban <dianahorban477@gmail.com>
2026-06-09 16:31:50 +0200

    The solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+set -e
 kind create cluster --config cluster.yml
 kubectl get nodes --show-labels
-MYSQL_NODE=$(kubectl get nodes -l app=mysql -o jsonpath='{.items[0].metadata.name}')
-kubectl taint nodes $MYSQL_NODE app=mysql:NoSchedule
-cd helm-chart/todoapp
+kubectl taint nodes -l app=mysql app=mysql:NoSchedule --overwrite
+cd .infrastructure/helm-chart/todoapp
 helm dependency build
-cd ../..
-helm upgrade --install todoapp ./helm-chart/todoapp --create-namespace --namespace todo-apps
-sleep 30
+cd ../../..
+helm upgrade --install todoapp ./.infrastructure/helm-chart/todoapp --create-namespace --namespace todo-apps
+kubectl wait --namespace todo-apps --for=condition=available --timeout=300s deployment -l app.kubernetes.io/name=todoapp || true
 kubectl get all,cm,secret,ing -A > output.log
